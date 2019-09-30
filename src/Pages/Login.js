6cc3deecd46bd1356b5f55b4666ad7c32e83ae3f
soucer10 <hugobicudo0@gmail.com>
2019-09-29 22:54:37 -0300

    Pagina de Login finalizada

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import './Login.css'
+
 function Login(){
 
-    return (<h1>A belinha é Linda, do Pai!</h1>);
+    return (<div className="login">
+            <img className="foto" alt="Foto" src="https://cdn-dpsp.pxos.com.br/logos/logo_dpsp_home.png"></img>
+            <form className='form' action="https://www.w3schools.com/html/html_forms.asp">
+            <input className="user" type='text' ></input>
+            <input className="user1" type='password'></input>
+            <button className="bt">Login</button>
+            </form>
+            </div>
+            );
 }
 
 export default Login;
